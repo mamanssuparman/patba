bc714c79fa7770310f54cfc07a63b1eb88927e82
mamanssuparman <mamanssuparman@gmail.com>
2022-09-06 15:04:57 +0700

    Layouts Admin

---
+++ resources/views/pages/admin/dashboard/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.Admin.kerangka')
+@section('conten')
+    <div class="row">
+        <div class="col-md-12">
+            ini adalah halaman dashboard
+        </div>
+    </div>
+@endsection
