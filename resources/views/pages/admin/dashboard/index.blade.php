67808c8181623f51025c53ec9e2a5f8abae1adfa
elfanhari <elfanhari88@gmail.com>
2022-09-17 13:35:59 +0700

    perubahan admin

--- resources/views/pages/admin/dashboard/index.blade.php
+++ resources/views/pages/admin/dashboard/index.blade.php
@@ -2,7 +2,7 @@
 @section('conten')
     <div class="row">
         <div class="col-md-12">
-            ini adalah halaman dashboard
+            ini adalah halaman dashboard adminwfefe
         </div>
     </div>
 @endsection
