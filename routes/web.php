9188f2ccdf998cea84af33a9db9dfd1cc72495a4
AdiApriyanto <94589309+Apriyanto20@users.noreply.github.com>
2022-10-22 12:10:49 +0700

    Routes adi

--- routes/web.php
+++ routes/web.php
@@ -31,29 +31,26 @@ Route::get('/admin/login', function () {
 Route::post('/admin/login-cek', [AuthAdminController::class, 'cek'])->name('auth-admin');
 
 // Route::middleware(['admin'])->group(function () {
-    Route::get('/admin/dashboard', function () {
-        return view('pages.admin.dashboard.index', [
-            'title'             => 'PATBA | Dashboaord',
-            'mainbreadcrumb'    => 'Dashboard',
-            'breadcrumb1'       => 'Dashboard',
-            'breadcrumb2'       => 'Index'
-        ]);
-    })->name('admin-dashboard');
-    Route::resource('/admin/matapelajaran', MataPelajaranController::class)->name('index', 'admin-matapelajaran')->except('show');
-    Route::any('/pelajaran/data',[DataJs::class,'dataPelajaran'])->name('datapelajaran');
-    
-    Route::resource('/admin/kelas', KelasController::class)->name('index', 'admin-kelas')->except('show');
-    Route::any('/kelas/data',[DataJs::class,'dataKelas'])->name('datakelas');
-<<<<<<< HEAD
+Route::get('/admin/dashboard', function () {
+    return view('pages.admin.dashboard.index', [
+        'title'             => 'PATBA | Dashboaord',
+        'mainbreadcrumb'    => 'Dashboard',
+        'breadcrumb1'       => 'Dashboard',
+        'breadcrumb2'       => 'Index'
+    ]);
+})->name('admin-dashboard');
+Route::resource('/admin/matapelajaran', MataPelajaranController::class)->name('index', 'admin-matapelajaran')->except('show');
+Route::any('/pelajaran/data', [DataJs::class, 'dataPelajaran'])->name('datapelajaran');
 
-    Route::get('/admin/soal', [SoalController::class, 'index'])->name('admin-soal');
-    Route::any('/soal/data',[DataJs::class, 'dataSoal'])->name('datasoal');
+Route::resource('/admin/kelas', KelasController::class)->name('index', 'admin-kelas')->except('show');
+Route::any('/kelas/data', [DataJs::class, 'dataKelas'])->name('datakelas');
+
+
+Route::get('/admin/soal', [SoalController::class, 'index'])->name('admin-soal');
+Route::any('/soal/data', [DataJs::class, 'dataSoal'])->name('datasoal');
 // });
-=======
-});
->>>>>>> ce47bbe16ba0bc98c06b93acfcba2848d1732d11
 
-    Route::get('/admin/dashboard',[DashboardController::class, 'index'])->name('admin-dashboard');
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin-dashboard');
 
 Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
     Lfm::routes();
